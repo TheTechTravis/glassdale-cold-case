c87f73e07b510307ce62533ac401a7fe1e9852ea
Travis Stevenson <thetechtravis@outlook.com>
2020-10-16 14:42:31 -0500

    Wrote getCriminals functionality in CriminalDataProvider.js file. The purpose of this function is to fetch data from the provided API, parse the data to JSON format, and use parsed data to fill the empty criminals[] array.

---
+++ scripts/criminals/CriminalDataProvider.js
@@ -0,0 +1,19 @@
+let criminals = []
+
+// Create a copy of criminals[] to work with so that the original array is not mutated
+export const useCriminals = () => {
+    return criminals.slice()
+}
+
+// Fetch data from the provided API, parse the data to JSON format, and use parsed data to fill the empty criminals[] array
+export const getCriminals = () => {
+    return fetch("https://criminals.glassdale.us/criminals")
+        .then(response => response.json())
+        .then(
+            parsedCriminals => {
+                console.table(parsedCriminals)
+                criminals = parsedCriminals
+            }
+        )
+}
+
