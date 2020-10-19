a3bdf4d4bb59f67076bdcddc5b03ab078d4794a2
Travis Stevenson <thetechtravis@outlook.com>
2020-10-19 12:28:21 -0500

    Removed unneccessary imports and function invocations within main.js file. Formatted main.js.

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,8 @@
 // import { getCriminals } from "./criminals/CriminalDataProvider.js"
 // getCriminals()
 
-import  { useCriminals } from "./criminals/CriminalDataProvider.js"
-import  { criminalList } from "./criminals/CriminalList.js"
-import { useOfficers } from "./officers/OfficerDataProvider.js"
+import { criminalList } from "./criminals/CriminalList.js"
 import { officerList } from "./officers/OfficerList.js"
 
-console.log("Welcome to the main module")
-
-
-// useCriminals()
 criminalList()
-
-// useOfficers()
 officerList()
