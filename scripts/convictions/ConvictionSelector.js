becd87ee5499b5e143f840c372093edca5d8f402
Travis Stevenson <thetechtravis@outlook.com>
2020-10-21 10:15:26 -0500

    Refactored code to match up with Jisies video.

--- scripts/convictions/ConvictionSelector.js
+++ scripts/convictions/ConvictionSelector.js
@@ -4,9 +4,6 @@
  */
 import { useConvictions, getConvictions } from "./ConvictionDataProvider.js"
 
-// Get a reference to the DOM element where the <select> will be rendered
-const contentTarget = document.querySelector(".filters__crime")
-
 export const ConvictionSelector = () => {
     // Get all convictions from application state
     
@@ -16,24 +13,49 @@ export const ConvictionSelector = () => {
     })
 }
 
+// Steve Brownlee's Code Starts Here!
+/*
+    Which element in your HTML contains all components?
+    That's your Event Hub. Get a reference to it here.
+*/
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".filters__crime")
+
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
 const render = convictionsCollection => {
-    /*
-        Use interpolation here to invoke the map() method on
-        the convictionsCollection to generate the option elements.
-        Look back at the example provided above.
-    */
     contentTarget.innerHTML = `
         <select class="dropdown" id="crimeSelect">
             <option value="0">Please select a crime...</option>
             ${
                 convictionsCollection.map(convictionsObj => {
                     const valueToAdd = convictionsObj.name
-                    return `
-                    <option> ${valueToAdd} </option>
-                    `
+                    return `<option> ${valueToAdd} </option>`
                     
-            })
-        }
+            })}
         </select>
     `
-        }
+}
+
+export const ConvictionSelect = () => {
+    getConvictions()
+        .then(() => {
+            const convictions = useConvictions()
+            render(convictions)
+        })
+}
