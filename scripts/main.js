c9a9ffefd7f533cc670622a6ebce92331e34be8e
Travis Stevenson <thetechtravis@outlook.com>
2020-10-22 15:18:00 -0500

    Added OfficerSelect.js module. Invoked OfficerSelect() in main.js. Formatted ConvictionSelect.js for readability.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { officerList } from "./officers/OfficerList.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 CriminalList()
 officerList()
 ConvictionSelect()
+OfficerSelect()
